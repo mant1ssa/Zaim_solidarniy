a585a66b29782298374f43a16fb436cf38c136de
mant1ssa <nayrix.8@gmail.com>
2022-08-26 02:06:19 +0500

    web-site: renewed everything

--- js/range.js
+++ js/range.js
@@ -7,9 +7,9 @@ slider.oninput = function(){
     output.innerHTML = this.value + " &#8381";
 }
 
-slider.addEventListener("mousemove", function(){
+slider.addEventListener("touchmove", function(){
     console.log('hey');
     var x = slider.value / 601;
-    var color = 'linear-gradient(90deg, red ' + x + '%, rgb(141, 141, 141) ' + x + '%)';
+    var color = 'linear-gradient(90deg, #04b586 ' + x + '%, rgb(202, 202, 202) ' + x + '%)';
     slider.style.background = color;
 })
